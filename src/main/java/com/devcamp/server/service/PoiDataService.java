a11845b624518aa4a97d89b69f59c749787496ed
samkiller <samkiller@gmail.com>
2012-02-18 22:19:04 +0100

    lll

--- src/main/java/com/devcamp/server/service/PoiDataService.java
+++ src/main/java/com/devcamp/server/service/PoiDataService.java
@@ -1,28 +1,56 @@
 package com.devcamp.server.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+
 import com.devcamp.server.model.PoiData;
 import com.devcamp.server.resources.ResponseData;
+import com.devcamp.server.util.InitDatabase;
 
 
 public class PoiDataService {
 	
-	
+	private List<PoiData> listData = InitDatabase.storeDocument();
 	
 	public PoiDataService(){
 	   	
 	}
 	
-	public List<ResponseData> findData(PoiData poiData){
-	  ResponseData responseData = new ResponseData();
+	public List<ResponseData> findData(PoiData poiData, double radius){
+	  
 		//extract all stuff in the range from MongoDB
+	    List<ResponseData> responses = new ArrayList<ResponseData>();
+	    for (PoiData poi : listData){
+	    	if (gpsDistance(poiData, poi) <= radius){
+	    		responses.add(new ResponseData(poi));
+	    	}
+	    }
+	    return responses;
 		
+	}
+	
+	public double gpsDistance(PoiData source, PoiData dest){
+		//d = R * (Pi/2 - ArcSin( sin(destLat) * sin(sourceLat) 
+		//+ cos(destLong - sourceLong) * cos(destLat) * cos(sourceLat)))
 		
+		double destLat = dest.getLatitude();
+		double destLong = dest.getLongitute();
+		double sourceLat = source.getLatitude();
+		double sourceLong = source.getLongitute();
 		
-		return null;
+		double d = 6378 * (Math.PI/2 - Math.asin( Math.sin(destLat) * Math.sin(sourceLat) 
+				+ Math.cos(destLong - sourceLong) * Math.cos(destLat) * Math.cos(sourceLat)));
 		
+		return d;
+	}
+	
+	public static void main(String[] args){
+		PoiDataService pds = new PoiDataService();
+//		//PoiData pdt = pds.gpsDistanc
+//		for (ResponseData rps: pds.findData(poiData, 100));
+//		
 	}
 
 	
--- src/main/java/com/devcamp/server/util/InitDatabase.java
+++ src/main/java/com/devcamp/server/util/InitDatabase.java
@@ -6,93 +6,86 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.net.UnknownHostException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import com.devcamp.server.model.PoiData;
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.Morphia;
-import com.google.gson.Gson;
+
 import com.mongodb.Mongo;
-import com.mongodb.MongoException;
+
 
 public class InitDatabase {
-	
+
 	public static Mongo mongo;
 	public  static Datastore ds ;
 	public static Morphia morphia;
 	private static final String DB_NAME="mongo-android";
 	private static final String URL_DB="localhost";
 	private static final int PORT =27017;
-	
-	
+
+
 	public static void init(){
 		try {
 			mongo = new Mongo(URL_DB, PORT);
 			morphia = new Morphia().map(PoiData.class); 
 			ds = morphia        
-			         .createDatastore(mongo, DB_NAME);
-		
+					.createDatastore(mongo, DB_NAME);
+
 		} catch (Exception e) {
-				throw new RuntimeException("Error Initializing MongoDB .." + e);
+			throw new RuntimeException("Error Initializing MongoDB .." + e);
 		}
-		
+
 	}
-	
+
 	/**
 	 * 
 	 */
-	public static void storeDocument(){
-	
+	public static List<PoiData> storeDocument(){
+        
+		List<PoiData> list = new ArrayList<PoiData>();
 		URL url = InitDatabase.class.getResource("/MH-Ile-de-France.txt-fr_utf8_monument_opendata.paris.csv");
 		File csvFile = new File(url.getFile());
-		 
+
 		BufferedReader buffer;
 		try {
-			 buffer = new BufferedReader(new FileReader(csvFile));
-			 String line;
-			 
-			 while ((line =buffer.readLine())!=null) {
-				 //System.out.println(line);
-				 String[] splitted = line.split("\\;");
-				 
-//				 for(String s: splitted){
-//					 PoiData poiData = new PoiData();
-//					 poiData.setTitre(splitted[0]);
-//					 poiData.setDescription(splitted[1]);
-//					 poiData.setAddresse(splitted[2]);
-//					 poiData.setLatitude(Double.valueOf((splitted[3])));
-//					 poiData.setLongitute((Double.valueOf((splitted[4]))));
-//					 poiData.setTitre(splitted[5]);
-//					 
-//					 System.out.println(poiData);
-//				 }
-				 System.out.println();
-				 
-				 
-			 }
-			 
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}catch (IOException e){
-			e.printStackTrace();
+			buffer = new BufferedReader(new FileReader(csvFile));
+			String line;
+
+			while ((line =buffer.readLine())!=null) {
+				
+				String[] splitted = line.split("\\t");
+				PoiData poiData= new PoiData();
+				 poiData.setTitre(splitted[0]);
+				 poiData.setDescription(splitted[1]);
+				 poiData.setAddresse(splitted[2]);
+				 poiData.setLatitude(Double.valueOf(splitted[3]));
+				 poiData.setLongitute(Double.valueOf(splitted[4]));
+				 poiData.setCategory(splitted[5]);
+				 list.add(poiData);
+				
+			}
 			
 		}
-		
-		
-		
-	}
-		
+
+	catch (FileNotFoundException e) {
 	
-	public static void main(String[] args){
-		//init();
-		storeDocument();
-//		String tt = "Eglise du Vieux-Saint-Ouen;L'église : inscription par arrêté du 6 juin 1933. . Moyen Age;Eglise du Vieux-Saint-Ouen  Saint-Ouen;48.7035638;-0.1805359;monument";
-//		String[] splitted =tt.split("\\;");
-//		for (String s  : splitted){
-//			System.out.println(s);
-//		}
+		e.printStackTrace();
+	}catch (IOException e){
+		e.printStackTrace();
+
 	}
+		
+	return list;
+}
+	public static void main(String[] args){
+		for (PoiData poidata :InitDatabase.storeDocument()){
+			System.out.println(poidata);
+		}
+		
+	}
+
 
 }
